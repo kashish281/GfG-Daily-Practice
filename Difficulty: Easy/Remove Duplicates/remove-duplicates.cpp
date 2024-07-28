acea7232c2805538626d48d656e977d37ff5a684
Kashish Sinha <130341436+kashish281@users.noreply.github.com>
2024-07-28 16:35:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates/remove-duplicates.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string removeDups(string str) {
+        // Your code goes here
+         string ans;
+        unordered_map<char, int> mp;
+        int n = str.size();
+        for(int i=0; i<n; i++){
+            int sz = mp.size();
+            mp[str[i]]++;
+            if(sz != mp.size()){
+                ans += str[i];
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.removeDups(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
