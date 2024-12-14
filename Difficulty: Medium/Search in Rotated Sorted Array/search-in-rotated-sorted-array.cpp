773cecc20cb4856074b5d086e3edbf541ebcffcb
Kashish Sinha <130341436+kashish281@users.noreply.github.com>
2024-12-14 15:06:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -4,25 +4,31 @@ using namespace std;
 
 
 // } Driver Code Ends
-
 class Solution {
   public:
     int search(vector<int>& arr, int key) {
-        int i=0;
-        int j=arr.size()-1;
-        while(i<=j){
-            int mid=(i+j)/2;
-            if(arr[mid]==key) return mid;
-            if(arr[mid]>=arr[i]){
-                if(key>=arr[i] && key<arr[mid] ) j=mid-1;
-                else i=mid+1;
-            }
-            else{
-                if(key>arr[mid] && key<=arr[j]) i=mid+1;
-                else j=mid-1;
-            }
-        }
-        return -1;
+       int n=arr.size();
+       int low=0;
+       int high=n-1;
+       while(low<=high){
+           int mid=(low+high)/2;
+           if(arr[mid]==key)
+           return mid;
+           else if(arr[mid]>=arr[low]){
+               if(arr[mid]>=key && arr[low]<=key)
+               high=mid-1;
+               else
+               low=mid+1;
+           }
+           else{
+               if(arr[mid]<=key && arr[high]>=key)
+               low=mid+1;
+               else
+               high=mid-1;
+           }
+       }
+       return -1;
+        
     }
 };
 
@@ -44,6 +50,7 @@ int main() {
         cin >> key;
         Solution ob;
         cout << ob.search(arr, key) << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
