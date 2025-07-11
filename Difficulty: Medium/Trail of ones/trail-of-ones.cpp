0371bf502c4dbe44eee824ad1be74f047cb348a5
Kashish Sinha <130341436+kashish281@users.noreply.github.com>
2025-07-11 19:21:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Trail of ones/trail-of-ones.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int countConsec(int n) {
+        // code here
+        return pow(2, n) - countWithNoPair(n);
+    }
+    
+    int countWithNoPair(int n){
+        return travel(1, n, true) + travel(1, n, false);
+    }
+    
+    int travel(int i, int n, bool prev){
+        if(i == n){
+            return 1;
+        }
+        if(prev){
+            return travel(i + 1, n, false); 
+        }else{
+            return travel(i + 1, n, true) + travel(i + 1, n, false);
+        }
+    }
+};
