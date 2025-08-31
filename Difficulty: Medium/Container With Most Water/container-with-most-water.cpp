8213a7f7060c0cbc4cf435ad3b5fc9842972793b
Kashish Sinha <130341436+kashish281@users.noreply.github.com>
2025-08-31 19:43:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        // code here
+            int left = 0;
+        int right = arr.size() - 1;
+        int area = 0;
+        
+        while(left<=right){
+            area = max(area, (right - left)*min(arr[right], arr[left]));
+            
+            if(arr[left]<arr[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+            
+        }
+        
+        return area;
+    }
+};
